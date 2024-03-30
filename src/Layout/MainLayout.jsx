3eef7961233f799cf99220f54893e71da90d536c
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-03-31 00:52:57 +0600

    Log out functionality inplemented, user can log out from clicking nav profile image

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Home/Navbar/Navbar";
 import Footer from "../Pages/Home/Footer/Footer";
+import { Toaster } from "react-hot-toast";
 
 
 const MainLayout = () => {
@@ -8,6 +9,10 @@ const MainLayout = () => {
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Toaster
+                position="top-right"
+                reverseOrder={false}
+            />
             <Footer></Footer>
         </div>
     );
--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BsCart2 } from "react-icons/bs";
+import useAuth from "../../../Components/Hooks/useAuth";
+import toast from "react-hot-toast";
+import Button from "../../../Shared/Html/Button";
 
 const Navbar = () => {
-    // const { user, logOut } = useAuth();
+    const { user, logOut } = useAuth();
     // const [cart] = useCarts();
 
     const navLinks = <>
@@ -12,21 +15,21 @@ const Navbar = () => {
         <li><Link className='font-semibold text-white' to='/'>Cart</Link></li>
         <li><Link className='font-semibold text-white' to='/'>Checkout</Link></li>
         <li><Link className='font-semibold text-white' to='/'>Dashboard</Link></li>
-        <li><Link className='font-semibold text-white' to='/login'>Login</Link></li>
+        <li><Link className='font-semibold text-white' to='/'>Login</Link></li>
 
     </>
 
 
 
-    // const handleLogOut = () => {
-    //     logOut()
-    //         .then(() => {
-    //             toast.success('Log Out successfully!');
-    //         })
-    //         .catch(() => {
-    //             toast.error("Something wrong. Please Try again")
-    //         })
-    // }
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                toast.success('Log Out successfully!');
+            })
+            .catch(() => {
+                toast.error("Something wrong. Please Try again")
+            })
+    }
     return (
         <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white px-4">
             <div className="navbar-start">
@@ -59,26 +62,26 @@ const Navbar = () => {
                 <div className="dropdown dropdown-left z-10">
                     <label tabIndex={0} className="m-5">
                         <div className="avatar">
-                            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                {/* {
+                            <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 cursor-pointer">
+                                {
                                     user ? <div><img className="mr-3 w-[40px] rounded-full" src={user.photoURL} alt="" /></div> : <FaUser className='mx-auto text-3xl'></FaUser>
-                                } */}
+                                }
                             </div>
                         </div>
                     </label>
-                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-red-400 rounded-box w-52">
+                    <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-gray-500 bg-opacity-70 rounded-box w-52">
                         <div className="text-center my-3">
                             <div className="avatar">
                                 <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                    {/* {
+                                    {
                                         user ? <div><img className="mr-3 w-[40px] rounded-full" src={user.photoURL} alt="" /></div> : <FaUser className='mx-auto text-3xl'></FaUser>
-                                    } */}
+                                    }
                                 </div>
                             </div>
-                            {/* <div className='my-5 text-xs text-white'>User Name : <br />{user ? <>{user.displayName}</> : <span className='text-black font-bold'>(You are not Log in)</span>}</div>
-                            <div className='my-5 text-xs text-white'>User Email : <br />{user ? <>{user.email}</> : <span className='text-black font-bold'>(You are not Log in)</span>}</div>
+                            <div className='my-5 text-xs text-white'>User Name : <br />{user ? <>{user.displayName}</> : <span className='text-red-500 font-bold'>(You are not Log in)</span>}</div>
+                            <div className='my-5 text-xs text-white'>User Email : <br />{user ? <>{user.email}</> : <span className='text-red-500 font-bold'>(You are not Log in)</span>}</div>
 
-                            <li><Link className='font-semibold text-white mx-auto' ><button onClick={handleLogOut} className='px-3 py-1 rounded-md bg-blue-500 hover:bg-orange-600 w-full'>Log Out</button></Link></li> */}
+                            <li><Link className='font-semibold text-white mx-auto' ><Button onClick={handleLogOut} className='px-3 py-1 bg-blue-500 hover:bg-orange-600 w-full'>Log Out</Button></Link></li>
                         </div>
                     </ul>
                 </div>
